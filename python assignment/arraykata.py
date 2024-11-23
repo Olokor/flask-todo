32c6da4c59f9900c0284bd6ee8517ab5dfc56f8f
wisdom olokor <olokorwisdom15@gmail.com>
2024-11-23 20:20:19 +0100

    test palindrome and 3 testCases complete

--- python assignment/arraykata.py
+++ python assignment/arraykata.py
@@ -1,20 +1,16 @@
 def get_max(array:list):
-	if type(array) is list:
-		if array_contain_only_numbers(array):
-			max_number = array[0]
-			for i in array:
-				if i > max_number:
-					max_number = i
-			return max_number
+	if type(array) is list and array_contain_only_numbers(array):
+		max_number = array[0]
+		for i in array:
+			if i > max_number:
+				max_number = i
+		return max_number
 	raise TypeError
 
 def reverse_list(array:list):
-	if type(array) is list:
-		reversed_array = []
-		for i in range((len(array)-1), -1, -1):
-			reversed_array.append(array[i])
-		return reversed_array
+	if type(array) is list and array_contain_only_numbers(array): return array[::-1]
 	raise TypeError
+
 def elementExist(element, array:list):
 	if type(array) is list:
 		if element in array:
@@ -42,13 +38,27 @@ def print_even_index_values(array:list):
 		return even_array
 	raise TypeError
 
-def array_contain_only_numbers(array)
+def array_contain_only_numbers(array):
 	for i in array:
 		if type(i) not in [int, float]:
 			return False
 	return True
+def sum_array_element(array:list):
+	if type(array) is list:
+		if array_contain_only_numbers:
+			sum = 0
+			for i in array:
+				sum += i
+		return sum
+	raise TypeError
 
-		
+def check_palindrome(text:str):
+	if type(text) is str:
+		palindrome = text[::-1]
+		if text == palindrome:
+			return palindrome
+		return "does not have a possible palindrome"
+	raise TypeError
 
 
 
--- python assignment/test_arraykata.py
+++ python assignment/test_arraykata.py
@@ -24,25 +24,25 @@ class TestArrayKata(TestCase):
 
 
 	def test_that_reverse_list_raised_error_if_list_not_passed(self):
-		self.assertRaises(TypeError, reverse_list, "67")
+		self.assertRaises(TypeError, reverse_list, [1,2,3,5,"5"])
 
 	def test_that_elementExist_exist(self):
 		elementExist(5, [1,2,3,4,5])
 
-	def test_that_elementExist_returns_correct_value(self):
+	def test_that_elementExist_returns_correct_result(self):
 		actual_result = elementExist(5, [1,2,3,4,5])
 		expected_result = True
 		self.assertEqual(actual_result, expected_result)
 
 
-	def test_that_elementExist_returns_correct_value(self):
+	def test_that_elementExist_raised_error_if_list_not_passed(self):
 		self.assertRaises(TypeError, elementExist, "5", "yer")
 
 	
 	def test_that_print_odd_index_values_exist(self):
 		print_odd_index_values([1,2,3,4])
 	
-	def test_that_print_odd_index_values_returns_correct_values(self):
+	def test_that_print_odd_index_values_returns_correct_result(self):
 		actual_result = print_odd_index_values([1,2,3,4,5,6,7,8,9])
 		expected_result = [2,4,6,8]
 		self.assertEqual(actual_result, expected_result)
@@ -55,12 +55,33 @@ class TestArrayKata(TestCase):
 		print_even_index_values([])
 
 
-	def test_that_print_even_index_values_return_correct_values(self):
+	def test_that_print_even_index_values_return_correct_result(self):
 		actual_result = print_even_index_values([1,2,3,4,5,6,7,8,9])
 		expected_result = [1,3,5,7,9]
 		self.assertEqual(actual_result, expected_result)
 
 	def test_that_print_even_index_values_raise_error_if_list_not_passed(self):
 		self.assertRaises(TypeError, print_even_index_values, "677")
+
+	def test_that_sum_array_element_exist(self):
+		sum_array_element([1,2,3])
+
+	def test_that_sum_array_element_return_correct_result(self):
+		actual_result = sum_array_element([1,3,5])
+		expected_result = 9
+		self.assertEqual(actual_result, expected_result)
+
+	def test_that_sum_array_element_raise_error_if_list_not_passed(self):
+		self.assertRaises(TypeError, sum_array_element, "567")
+
+	def test_that_check_palindrome_exists(self):
+		check_palindrome("aba")
+	
+	def test_that_check_palindrome_return_correct_result(self):
+		actual_result = check_palindrome("aba")
+		expected_result = "aba"
+		self.assertEqual(actual_result, expected_result)
+
+
 
 		
