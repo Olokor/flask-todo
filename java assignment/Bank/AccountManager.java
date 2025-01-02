6171a3591d114ed9767a1bad8131f8b0ba34a793
Wisdom Olokor <olokorwisdom15@gmail.com>
2025-01-02 17:43:19 +0000

    bank simulation

--- java assignment/Bank/AccountManager.java
+++ java assignment/Bank/AccountManager.java
@@ -1,7 +1,6 @@
 
 import java.util.ArrayList;
 
-
 public class AccountManager{
     private ArrayList<CreateAccount> accounts = new ArrayList<>();
 
--- java assignment/Diary/Diary.java
+++ java assignment/Diary/Diary.java
@@ -1,12 +1,46 @@
 public class Diary{
     private String title;
     private String date;
-    private String body;
+    private String entry;
     private String passKey;
 
-    public Diary(String title, String date, String body, String passKey){
+    public Diary(String title, String date, String entry, String passKey){
         this.title = title;
         this.date = date;
-        this.body = body;
+        this.entry = entry;
     }
+
+    @Override
+    public String  toString(){
+        return String.format("%s %n%s %n%s", this.title, this.date, this.entry);
+    }
+
+    public String getPassKey(){
+        return this.passKey;
+    }
+
+    public String getDate(){
+        return this.date;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getEntry(){
+        return this.entry;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void addEntry(String entry){
+        this.entry += entry;
+    }
+
+    public void setEntry(String entry){
+        this.entry = entry;
+    }
+    
 }
