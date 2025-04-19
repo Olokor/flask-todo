9bf6d665395da4f3103176dd540fc19bc3c6089f
Wisdom Olokor <olokorwisdom15@gmail.com>
2025-04-19 14:26:10 +0000

    flask todo Application

---
+++ TODO-LIST/app.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Task {self.id}>'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():  # put application's code here
+    if request.method == 'POST':
+        new_task = request.form['content']
+        new_task = Todo(content=new_task)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was some issues"
+    else:
+        task = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=task)
+    # return render_template('index.html')
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was some issues"
+
+@app.route("/update/<int:id>", methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue updating your task"  
+    else:
+        return render_template('update.html', task=task)
+
+
+# def create_tables():
+#     with app.app_context():
+#         db.create_all()
+
+if __name__ == '__main__':
+    # create_tables()  # This will create the database when you run the app
+    app.run(debug=True)
