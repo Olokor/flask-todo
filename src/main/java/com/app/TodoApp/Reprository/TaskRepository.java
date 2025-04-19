467aa72b3cb31c0a58e7a4868db783d8fb266280
Wisdom Olokor <olokorwisdom15@gmail.com>
2025-04-19 14:34:49 +0000

    reqirement.txt

--- src/main/java/com/app/TodoApp/Reprository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.TodoApp.Reprository;
-
-import com.app.TodoApp.models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
